e7c226861831c151558054e059dbcd4d1ca6da3f
Kaylee Funk <kayleefunkee@gmail.com>
2025-05-12 19:30:47 -0700

    Add project files.

---
+++ ToDoApplication/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult UserRegistration()
+        {
+            return View();
+        }
+        public IActionResult LogInUser()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult LogOutUser()
+        {
+            return RedirectToAction("Login");
+        }
+        public IActionResult RegisterUser()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ ToDoApplication/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoApplication.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; } // foreign key
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
