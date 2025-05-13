43b03a8bbbb9d28813bafa8995e72c2ab0403329
Kaylee Funk <kayleefunkee@gmail.com>
2025-05-12 21:09:50 -0700

    Updated readme, created dbcontext, added entityframeworkcore and inmemory nuget packages

--- ToDoApplication/Controllers/AccountController.cs
+++ ToDoApplication/Controllers/AccountController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApplication.Data;
+using ToDoApplication.Models;
 
 namespace ToDoApplication.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ILogger<AccountController> _logger;
+        private readonly ToDoAppDbContext _context;
+
+        public AccountController(ILogger<AccountController> logger, ToDoAppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
         public IActionResult Login()
         {
             return View();
@@ -23,6 +34,12 @@ namespace ToDoApplication.Controllers
         public IActionResult RegisterUser()
         {
             return RedirectToAction("Login");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
